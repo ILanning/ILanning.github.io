e916be1cc51ea770e0e76489333e8dc450ef0d2a
Ian Lanning <ianclanning@gmail.com>
2017-06-25 02:10:15 -0700

    Built out header

--- scripts/angular_main.js
+++ scripts/angular_main.js
@@ -1,8 +1,16 @@
-var app = angular.module("portfolioApp", ["ngRoute"]);
+var app = angular.module("portfolioApp", ["ngRoute", "ngAnimate"]);
 
 app.config(function($routeProvider){
     $routeProvider.when('/', { templateUrl : "partials/main.html" })
                   .when("/outpost", { templateUrl : "partials/outpost.html" })
                   .when("/sessionviewer", { templateUrl : "partials/sessionviewer.html" })
+                  .when("/other", { templateUrl : "partials/other.html" })
                   .otherwise({redirectTo : '/'});
+});
+
+app.controller("mainController", function($scope, $routeParams, $location){
+    if($location.path() == "/"){
+        console.log("Main hit");
+        document.getElementById("topNavMainLink").focus();
+    }
 });
